7f19b363b935897c420a301a418e2db0f85b99bb
TOXYGENCY <delorean2026@ya.ru>
2026-01-12 11:40:13 +0300

    feat: связь с экспортом диплома. - Добавлен контроллер для принятия запроса на экспорт диплома - Добавлен метод передачи данных запроса в сервис экспорта

--- Dekauto.Students.Service/Students.Service/Domain/Entities/Adapters/DiplomaSupplementData.cs
+++ Dekauto.Students.Service/Students.Service/Domain/Entities/Adapters/DiplomaSupplementData.cs
@@ -1,12 +1,20 @@
-﻿namespace Dekauto.Students.Service.Students.Service.Domain.Entities.Adapters
+﻿using Dekauto.Students.Service.Students.Service.Domain.Entities.DTO;
+
+namespace Dekauto.Students.Service.Students.Service.Domain.Entities.Adapters
 {
     public class DiplomaSupplementData
     {
-        public bool? diplomaWithHonors { get; set; } // с отличием или нет
-        public string? surname { get; set; } // фамилия
-        public string? Name { get; set; } // имя
-        public string? Patronymic { get; set; } // отчество
-        public bool? Gender { get; set; } // пол
-        public DateOnly? BirthdayDate { get; set; } // дата рождения
+        public bool? DiplomaWithHonors { get; set; } // с отличием
+        public string? Name { get; set; } // Имя
+        public string? Surname { get; set; } // Фамилия
+        public string? Patronymic { get; set; } // Отчество
+        public DateOnly? BirthdayDate { get; set; } // Дата рождения
+        public string? EducationReceived { get; set; } // Наименование документа о предыдущем образовании
+        public DateOnly? EducationReceivedDate { get; set; } // Год выдачи документа образования
+
+        // Наименования дисциплин (модулей), практик, курсовых работ
+        // +Количество зачетных единиц / академических часов / астрономических часов
+        // +Оценка
+        public List<DisciplineGradeDto> DisciplineResults { get; set; } = new();
     }
 }
